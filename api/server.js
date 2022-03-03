eee4de52b922929bc342931494817b46d37dc273
Josh Manik <josh.manik@dynamicmobilebilling.com>
2022-03-03 12:17:47 +0000

    reverted back to local for easier workflow

--- api/server.js
+++ api/server.js
@@ -29,7 +29,7 @@ const DB = mysql.createConnection({
     user : process.env.DB_USER,
     password : process.env.DB_PASSWORD,
     database : process.env.DB_NAME,
-    port : 3306
+    port : process.env.DB_PORT
 })
 
 DB.connect()
